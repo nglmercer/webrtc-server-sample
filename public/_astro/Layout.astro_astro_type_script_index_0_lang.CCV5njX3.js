bbe8b7e4dd85888d18ea131f679386664607d73b
melserngl <128845117+nglmercer@users.noreply.github.com>
2025-07-25 20:46:59 -0600

    add build

---
+++ public/_astro/Layout.astro_astro_type_script_index_0_lang.CCV5njX3.js
@@ -0,0 +1,1 @@
+import"https://cdn.socket.io/4.7.5/socket.io.min.js";
--- src/example.js
+++ src/example.js
@@ -2,8 +2,7 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
-import signaling_server from './signaling_server';
-
+import { signaling_server } from 'webrtc-socket-api';
 const app = express();
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
@@ -11,10 +10,9 @@ const io = new Server(httpServer, {
         origin: "*",
     }
 });
-
+app.use(express.static('public'));
 const PORT = process.env.PORT || 9001
 
-app.use(express.static('public'));
 
 io.on('connection', (socket) => {
     console.log(`\n[Server] New user connected with socket ID: ${socket.id}`);
