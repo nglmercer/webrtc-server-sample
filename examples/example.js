3d8933ad8711c9de9bf6661c6439f88dba479d18
melserngl <128845117+nglmercer@users.noreply.github.com>
2025-11-12 22:55:00 -0500

    fix import package

--- examples/example.js
+++ examples/example.js
@@ -9,7 +9,7 @@ import {
   defaultLogger as logger,
   getHeartbeatConfig,
   SocketIOLikeServer,
-} from "webrtc-socket-api"; //prod=  'webrtc-socket-api'  || dev= './index' ../src/signal_server'
+} from "../dist/index.js"; //prod=  'webrtc-socket-api'  || dev= './index' ../src/signal_server'
 //import { defaultLogger } from 'ws-socketio-adapter';
 //import { SocketIOLikeSocket } from '../src/adapters/SocketIOLikeSocket';
 //import { defaultLogger as logger } from '../src/logger';
